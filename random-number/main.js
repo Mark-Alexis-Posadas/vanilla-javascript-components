1c9a288de4f6e9c386878786251c99a7718058cc
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2023-09-17 20:49:25 +0800

    done random number with bg colors

--- random-number/main.js
+++ random-number/main.js
@@ -2,10 +2,25 @@ const number = document.getElementById("random");
 const getRandomNumberButton = document.getElementById("get_random_number");
 
 const handleClick = () => {
+  //number
   const minNumber = 1;
   const maxNumber = 100;
-  return (number.innerHTML =
-    Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber);
+
+  number.innerHTML =
+    Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+
+  //colors
+
+  let maxVal = 0xffffff; // 16777215
+  let randomNumber = Math.random() * maxVal;
+  randomNumber = Math.floor(randomNumber);
+  randomNumber = randomNumber.toString(16);
+  let randColor = randomNumber.padStart(6, 0);
+  `#${randColor.toUpperCase()}`;
+
+  document.querySelector(
+    "body"
+  ).style.background = `#${randColor.toUpperCase()}`;
 };
 
 getRandomNumberButton.addEventListener("click", handleClick);
