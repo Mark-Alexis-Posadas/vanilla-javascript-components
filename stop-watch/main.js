76e68331f4808b3756101903c082fe53cb057ac4
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2023-09-17 17:53:56 +0800

    added stop watch

---
+++ stop-watch/main.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll(".buttons");
+let currentCount = 0;
+let intervalId;
+const updateCount = () => {
+  currentCount++;
+  document.getElementById("seconds").innerHTML = currentCount;
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.id === "start") {
+      // Call updateCount every 1000 milliseconds (1 second)
+      intervalId = setInterval(updateCount, 10);
+    } else if (button.id === "reset") {
+      clearInterval(intervalId);
+      currentCount = 0;
+      document.getElementById("seconds").innerHTML = currentCount;
+    } else if (button.id === "stop") {
+      clearInterval(intervalId);
+    }
+  });
+});
