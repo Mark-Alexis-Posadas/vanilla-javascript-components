b860ddbe824135432d7719f8df8c2a2348286ff6
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2023-09-19 19:45:01 +0800

    Added tab

--- calculator/main.js
+++ calculator/main.js
@@ -1,32 +1,90 @@
-// // program for a simple calculator
-// let result;
+const calculatorButtons = document.querySelectorAll(".calculator-buttons");
+const calculatorInput = document.getElementById("input");
+let number1 = "";
+let operator = "";
+let number2 = "";
 
-// // take the operator input
-// const operator = prompt("Enter operator ( either +, -, * or / ): ");
+calculatorButtons.forEach((button) => {
+  const handleClick = () => {
+    const buttonText = button.textContent;
 
-// // take the operand input
-// const number1 = parseFloat(prompt("Enter first number: "));
-// const number2 = parseFloat(prompt("Enter second number: "));
+    if (isNumber(buttonText)) {
+      if (operator === "") {
+        number1 += buttonText;
+      } else {
+        number2 += buttonText;
+      }
+    } else if (isOperator(buttonText)) {
+      if (number1 !== "") {
+        operator = buttonText;
+      } else {
+        console.log("Invalid input: Please enter a number first.");
+      }
+    } else if (buttonText === "=") {
+      if (number1 !== "" && number2 !== "") {
+        calculateResult();
+      } else {
+        console.log(
+          "Invalid input: Please enter both numbers and an operator."
+        );
+      }
+    } else if (buttonText === "c") {
+      clearInput();
+    }
 
-// switch (operator) {
-//   case "+":
-//     result = number1 + number2;
-//     console.log(`${number1} + ${number2} = ${result}`);
-//     break;
-//   case "-":
-//     result = number1 - number2;
-//     console.log(`${number1} - ${number2} = ${result}`);
-//     break;
-//   case "*":
-//     result = number1 * number2;
-//     console.log(`${number1} * ${number2} = ${result}`);
-//     break;
-//   case "/":
-//     result = number1 / number2;
-//     console.log(`${number1} / ${number2} = ${result}`);
-//     break;
+    calculatorInput.value = `${number1} ${operator} ${number2}`;
+  };
 
-//   default:
-//     console.log("Invalid operator");
-//     break;
-// }
+  button.addEventListener("click", handleClick);
+});
+
+function isNumber(value) {
+  return !isNaN(value);
+}
+
+function isOperator(value) {
+  return value === "+" || value === "-" || value === "*" || value === "/";
+}
+
+function calculateResult() {
+  const num1 = parseFloat(number1);
+  const num2 = parseFloat(number2);
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      if (num2 !== 0) {
+        result = num1 / num2;
+      } else {
+        console.log("Division by zero is not allowed.");
+        clearInput();
+        return;
+      }
+      break;
+    default:
+      console.log("Invalid operator");
+      clearInput();
+      return;
+  }
+
+  calculatorInput.value = result;
+  number1 = result.toString();
+  number2 = "";
+  operator = "";
+}
+
+function clearInput() {
+  calculatorInput.value = "";
+  number1 = "";
+  operator = "";
+  number2 = "";
+}
